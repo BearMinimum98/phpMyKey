7e23497c6206f7a837b426906f9d8a9a49dfc399
KevZho <KevZho141@gmail.com>
2013-10-05 19:25:27 -0700

    add in quotes around value for XHTML standards

--- viewkeys.php
+++ viewkeys.php
@@ -1,12 +1,13 @@
 <?php
 	include_once "config.php";
 	if ($_GET["key"] == $cronKey) {
-		echo "<select multiple='multiple' size='10' style='width:200px;'>";
+		echo "<select multiple='multiple' size='10' style='width:200px;' id='keyslist'>";
 		foreach($keys as $currentKey) {
 			if ($currentKey != "")
-				echo "<option value=$currentKey>$currentKey</option>";
+				echo "<option value='$currentKey'>$currentKey</option>";
 		}
 		echo "</select><br />Total keys: " . (count($keys) - 1);
+		//add addkey and removekey functionality here
 	} else {
 		echo $keyRemoveIncorrect;
 	}
