17ed5f60871fe69bbf4a81a206fe1895e388d3f7
Kevin Zhou <KevZho141@gmail.com>
2013-10-09 10:00:33 -0700

    Update changecron.php

--- changecron.php
+++ changecron.php
@@ -1,4 +1,5 @@
 <?php
+	//Edit so cronkey is also in the db instead of a file on the server.
 	include_once "config.php";
 	if ($_POST["key"] == $cronKey and strlen($_POST["new"]) > 0) {
 		file_put_contents($cronKeyFile, $_POST["new"]);
